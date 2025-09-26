d9ac061ff55f6600e9cb8d037e6da9a46b01eb3e
Exstee <exsteeyo@gmail.com>
2025-09-26 08:06:12 -0400

    fix: Resolve ambiguous column reference in element query

--- element.sh
+++ element.sh
@@ -6,7 +6,11 @@ fi
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-ELEMENT=$($PSQL "SELECT atomic_number, name, symbol FROM elements WHERE atomic_number::text='$1' OR symbol='$1' OR name='$1';")
+ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius
+FROM elements
+JOIN properties USING(atomic_number)
+JOIN types USING(type_id)
+WHERE atomic_number::text='$1' OR symbol='$1' OR name='$1';")
 
 if [[ -z "$ELEMENT" ]]; then
   echo "I could not find that element in the database."
