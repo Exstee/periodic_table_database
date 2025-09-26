b7f6d026fcd6ce3298c2d06132a34b9905680701
Exstee <exsteeyo@gmail.com>
2025-09-26 07:39:24 -0400

    feat: Implement database query logic

--- element.sh
+++ element.sh
@@ -3,3 +3,12 @@ if [[ -z "$1" ]]; then
   echo "Please provide an element as an argument."
   exit
 fi
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+ELEMENT=$($PSQL "SELECT atomic_number, name, symbol FROM elements WHERE atomic_number::text='$1' OR symbol='$1' OR name='$1';")
+
+if [[ -z "$ELEMENT" ]]; then
+  echo "I could not find that element in the database."
+  exit
+fi
